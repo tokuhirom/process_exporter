561c59d4569e05aace942b87fe88232ea27ab8c7
Your Name <you@example.com>
2016-05-11 20:24:57 -0400

    Collect values when got request

--- process_exporter.go
+++ process_exporter.go
@@ -9,6 +9,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -38,190 +39,44 @@ var version string
 
 var (
 	addr       = flag.String("listen-address", ":9011", "The address to listen on for HTTP requests.")
-	interval   = flag.Duration("interval", 1*time.Second, "The interval for polling.")
 	filter     = flag.String("filter", "", "Commandline filter")
 	versionFlg = flag.Bool("version", false, "Show version number")
+	verboseFlg = flag.Bool("verbose", false, "verbose")
 )
 
-var (
-	labelNames = []string{"pid", "comm", "cmdline"}
+const namespace = "proc"
 
-	ioRCharGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procio_rchar",
-			Help: "characters read",
-		},
-		labelNames,
-	)
-	ioWCharGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procio_wchar",
-			Help: "characters written",
-		},
-		labelNames,
-	)
-	ioSyscRGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procio_syscr",
-			Help: "read syscalls",
-		},
-		labelNames,
-	)
-	ioSyscWGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procio_syscw",
-			Help: "write syscalls",
-		},
-		labelNames,
-	)
-	ioReadBytesGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procio_readbytes",
-			Help: "bytes read",
-		},
-		labelNames,
-	)
-	ioWriteBytesGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procio_writebytes",
-			Help: "bytes written",
-		},
-		labelNames,
-	)
-	ioCancelledWriteBytesGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procio_cancelledwritebytes",
-			Help: "cancelled write bytes",
-		},
-		labelNames,
-	)
+type Exporter struct {
+	mutex       sync.RWMutex
+	filterRegex *regexp.Regexp
+	bootTime    float64
+	pagesize    int
 
-	minfltGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_minflt",
-			Help: "The number of minor faults the process has made which have not required loading a memory page from disk",
-		},
-		labelNames,
-	)
-	cminfltGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_cminflt",
-			Help: "The number of minor faults that the process's waited-for children have made",
-		},
-		labelNames,
-	)
-	majfltGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_majflt",
-			Help: "The number of major faults that the process's waited-for children have made",
-		},
-		labelNames,
-	)
-	cmajfltGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_cmajflt",
-			Help: "The number of major faults that the process's waited-for children have made",
-		},
-		labelNames,
-	)
-	utimeGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_utime",
-			Help: "Amount of time that this process has been scheduled in user mode, measured in clock ticks.",
-		},
-		labelNames,
-	)
-	stimeGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_stime",
-			Help: "Amount of time that this process has been scheduled in kernel mode, measured in clock ticks.",
-		},
-		labelNames,
-	)
-	cutimeGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_cutime",
-			Help: "Amount of time that this process's waited-for children have been scheduled in user mode, measured in clock ticks.",
-		},
-		labelNames,
-	)
-	cstimeGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_cstime",
-			Help: "Amount of time that this process's waited-for children have been scheduled in kernel mode, measured in clock ticks.",
-		},
-		labelNames,
-	)
-	niceGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_nice",
-			Help: "The nice value, a value in the range 19 (low priority) to -20 (high priority)",
-		},
-		labelNames,
-	)
-	numThreadsGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_numthreads",
-			Help: "Number of threads in this process",
-		},
-		labelNames,
-	)
-	startTimeGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_starttime_epoch",
-			Help: "The time the process started in epoch seconds",
-		},
-		labelNames,
-	)
-	vsizeGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_vsize",
-			Help: "Virtual memory size in bytes",
-		},
-		labelNames,
-	)
-	rssGauge = prometheus.NewGaugeVec(
-		prometheus.GaugeOpts{
-			Name: "procstat_rss",
-			Help: "Resident set size in bytes",
-		},
-		labelNames,
-	)
-)
+	scrapeFailures prometheus.Counter
 
-func init() {
-	prometheus.MustRegister(ioRCharGauge)
-	prometheus.MustRegister(ioWCharGauge)
-	prometheus.MustRegister(ioSyscRGauge)
-	prometheus.MustRegister(ioSyscWGauge)
-	prometheus.MustRegister(ioReadBytesGauge)
-	prometheus.MustRegister(ioWriteBytesGauge)
-	prometheus.MustRegister(ioCancelledWriteBytesGauge)
-
-	prometheus.MustRegister(minfltGauge)
-	prometheus.MustRegister(cminfltGauge)
-	prometheus.MustRegister(majfltGauge)
-	prometheus.MustRegister(cmajfltGauge)
-	prometheus.MustRegister(utimeGauge)
-	prometheus.MustRegister(stimeGauge)
-	prometheus.MustRegister(cutimeGauge)
-	prometheus.MustRegister(cstimeGauge)
-	prometheus.MustRegister(niceGauge)
-	prometheus.MustRegister(numThreadsGauge)
-	prometheus.MustRegister(startTimeGauge)
-	prometheus.MustRegister(vsizeGauge)
-	prometheus.MustRegister(rssGauge)
+	ioRCharGauge               *prometheus.GaugeVec
+	ioWCharGauge               *prometheus.GaugeVec
+	ioSyscRGauge               *prometheus.GaugeVec
+	ioSyscWGauge               *prometheus.GaugeVec
+	ioReadBytesGauge           *prometheus.GaugeVec
+	ioWriteBytesGauge          *prometheus.GaugeVec
+	ioCancelledWriteBytesGauge *prometheus.GaugeVec
+	minfltGauge                *prometheus.GaugeVec
+	cminfltGauge               *prometheus.GaugeVec
+	majfltGauge                *prometheus.GaugeVec
+	cmajfltGauge               *prometheus.GaugeVec
+	utimeGauge                 *prometheus.GaugeVec
+	stimeGauge                 *prometheus.GaugeVec
+	cutimeGauge                *prometheus.GaugeVec
+	cstimeGauge                *prometheus.GaugeVec
+	niceGauge                  *prometheus.GaugeVec
+	numThreadsGauge            *prometheus.GaugeVec
+	startTimeGauge             *prometheus.GaugeVec
+	vsizeGauge                 *prometheus.GaugeVec
+	rssGauge                   *prometheus.GaugeVec
 }
 
-func main() {
-	flag.Parse()
-
-	if *versionFlg {
-		fmt.Fprintf(os.Stderr, "%s version %s\n", os.Args[0], version)
-
-		os.Exit(0)
-	}
-
+func NewExporter(filter *string) (*Exporter, error) {
 	var filterRegex *regexp.Regexp = nil
 	if filter != nil {
 		log.Printf("Filter: %s\n", *filter)
@@ -234,73 +89,309 @@ func main() {
 
 	procStat, err := procfs.NewStat()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	bootTime := float64(procStat.BootTime)
-	pagesize := os.Getpagesize()
 
-	go func() {
-		for {
-			procs, err := procfs.AllProcs()
-			if err != nil {
-				log.Print(err)
+	labelNames := []string{"pid", "comm", "cmdline"}
+
+	return &Exporter{
+		filterRegex: filterRegex,
+		bootTime:    float64(procStat.BootTime),
+		pagesize:    os.Getpagesize(),
+		scrapeFailures: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "exporter_scrape_failures_total",
+			Help:      "Number of errors while scraping apache.",
+		}),
+
+		ioRCharGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procio_rchar",
+				Help: "characters read",
+			},
+			labelNames,
+		),
+		ioWCharGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procio_wchar",
+				Help: "characters written",
+			},
+			labelNames,
+		),
+		ioSyscRGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procio_syscr",
+				Help: "read syscalls",
+			},
+			labelNames,
+		),
+		ioSyscWGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procio_syscw",
+				Help: "write syscalls",
+			},
+			labelNames,
+		),
+		ioReadBytesGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procio_readbytes",
+				Help: "bytes read",
+			},
+			labelNames,
+		),
+		ioWriteBytesGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procio_writebytes",
+				Help: "bytes written",
+			},
+			labelNames,
+		),
+		ioCancelledWriteBytesGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procio_cancelledwritebytes",
+				Help: "cancelled write bytes",
+			},
+			labelNames,
+		),
+
+		minfltGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_minflt",
+				Help: "The number of minor faults the process has made which have not required loading a memory page from disk",
+			},
+			labelNames,
+		),
+		cminfltGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_cminflt",
+				Help: "The number of minor faults that the process's waited-for children have made",
+			},
+			labelNames,
+		),
+		majfltGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_majflt",
+				Help: "The number of major faults that the process's waited-for children have made",
+			},
+			labelNames,
+		),
+		cmajfltGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_cmajflt",
+				Help: "The number of major faults that the process's waited-for children have made",
+			},
+			labelNames,
+		),
+		utimeGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_utime",
+				Help: "Amount of time that this process has been scheduled in user mode, measured in clock ticks.",
+			},
+			labelNames,
+		),
+		stimeGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_stime",
+				Help: "Amount of time that this process has been scheduled in kernel mode, measured in clock ticks.",
+			},
+			labelNames,
+		),
+		cutimeGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_cutime",
+				Help: "Amount of time that this process's waited-for children have been scheduled in user mode, measured in clock ticks.",
+			},
+			labelNames,
+		),
+		cstimeGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_cstime",
+				Help: "Amount of time that this process's waited-for children have been scheduled in kernel mode, measured in clock ticks.",
+			},
+			labelNames,
+		),
+		niceGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_nice",
+				Help: "The nice value, a value in the range 19 (low priority) to -20 (high priority)",
+			},
+			labelNames,
+		),
+		numThreadsGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_numthreads",
+				Help: "Number of threads in this process",
+			},
+			labelNames,
+		),
+		startTimeGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_starttime_epoch",
+				Help: "The time the process started in epoch seconds",
+			},
+			labelNames,
+		),
+		vsizeGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_vsize",
+				Help: "Virtual memory size in bytes",
+			},
+			labelNames,
+		),
+		rssGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "procstat_rss",
+				Help: "Resident set size in bytes",
+			},
+			labelNames,
+		),
+	}, nil
+}
+
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	e.scrapeFailures.Describe(ch)
+
+	e.ioRCharGauge.Describe(ch)
+	e.ioWCharGauge.Describe(ch)
+	e.ioSyscRGauge.Describe(ch)
+	e.ioSyscWGauge.Describe(ch)
+	e.ioReadBytesGauge.Describe(ch)
+	e.ioWriteBytesGauge.Describe(ch)
+	e.ioCancelledWriteBytesGauge.Describe(ch)
+	e.minfltGauge.Describe(ch)
+	e.cminfltGauge.Describe(ch)
+	e.majfltGauge.Describe(ch)
+	e.cmajfltGauge.Describe(ch)
+	e.utimeGauge.Describe(ch)
+	e.stimeGauge.Describe(ch)
+	e.cutimeGauge.Describe(ch)
+	e.cstimeGauge.Describe(ch)
+	e.niceGauge.Describe(ch)
+	e.numThreadsGauge.Describe(ch)
+	e.startTimeGauge.Describe(ch)
+	e.vsizeGauge.Describe(ch)
+	e.rssGauge.Describe(ch)
+}
+
+func (e *Exporter) collect(ch chan<- prometheus.Metric) error {
+	procs, err := procfs.AllProcs()
+	if err != nil {
+		return err
+	}
+
+	for _, proc := range procs {
+		stat, err := proc.NewStat()
+		if err != nil {
+			dbg(err)
+			continue
+		}
+
+		cmdline, err := proc.CmdLine()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		sCmdline := strings.Join(cmdline, " ")
+
+		if e.filterRegex != nil {
+			if !e.filterRegex.MatchString(sCmdline) {
 				continue
 			}
-			for _, proc := range procs {
-				stat, err := proc.NewStat()
-				if err != nil {
-					// log.Print(err)
-					continue
-				}
-
-				cmdline, err := proc.CmdLine()
-				if err != nil {
-					log.Print(err)
-					continue
-				}
-				sCmdline := strings.Join(cmdline, " ")
-
-				if filterRegex != nil {
-					if !filterRegex.MatchString(sCmdline) {
-						continue
-					}
-				}
-
-				labels := []string{strconv.Itoa(proc.PID), stat.Comm, sCmdline}
-
-				io, err := proc.NewIO()
-				if err != nil {
-					// This makes lot of noisy logging output when this process run on non-root user.
-					// log.Print(err)
-				} else {
-					ioRCharGauge.WithLabelValues(labels...).Set(float64(io.RChar))
-					ioWCharGauge.WithLabelValues(labels...).Set(float64(io.WChar))
-					ioSyscRGauge.WithLabelValues(labels...).Set(float64(io.SyscR))
-					ioSyscWGauge.WithLabelValues(labels...).Set(float64(io.SyscW))
-					ioReadBytesGauge.WithLabelValues(labels...).Set(float64(io.ReadBytes))
-					ioWriteBytesGauge.WithLabelValues(labels...).Set(float64(io.WriteBytes))
-					ioCancelledWriteBytesGauge.WithLabelValues(labels...).Set(float64(io.CancelledWriteBytes))
-				}
-
-				// metrics data from /proc/$$/stat
-				// see https://github.com/prometheus/procfs/blob/master/proc_stat.go
-				minfltGauge.WithLabelValues(labels...).Set(float64(stat.MinFlt))
-				cminfltGauge.WithLabelValues(labels...).Set(float64(stat.CMinFlt))
-				majfltGauge.WithLabelValues(labels...).Set(float64(stat.MajFlt))
-				cmajfltGauge.WithLabelValues(labels...).Set(float64(stat.CMajFlt))
-				utimeGauge.WithLabelValues(labels...).Set(float64(stat.UTime))
-				stimeGauge.WithLabelValues(labels...).Set(float64(stat.STime))
-				cutimeGauge.WithLabelValues(labels...).Set(float64(stat.CUTime))
-				cstimeGauge.WithLabelValues(labels...).Set(float64(stat.CSTime))
-				niceGauge.WithLabelValues(labels...).Set(float64(stat.Nice))
-				numThreadsGauge.WithLabelValues(labels...).Set(float64(stat.NumThreads))
-				startTimeGauge.WithLabelValues(labels...).Set(bootTime + float64(stat.Starttime)/userHZ)
-				vsizeGauge.WithLabelValues(labels...).Set(float64(stat.VSize))
-				rssGauge.WithLabelValues(labels...).Set(float64(stat.RSS * pagesize))
-			}
-			time.Sleep(*interval)
 		}
-	}()
+
+		labels := []string{strconv.Itoa(proc.PID), stat.Comm, sCmdline}
+
+		io, err := proc.NewIO()
+		if err != nil {
+			dbg(err)
+		} else {
+			e.ioRCharGauge.WithLabelValues(labels...).Set(float64(io.RChar))
+			e.ioWCharGauge.WithLabelValues(labels...).Set(float64(io.WChar))
+			e.ioSyscRGauge.WithLabelValues(labels...).Set(float64(io.SyscR))
+			e.ioSyscWGauge.WithLabelValues(labels...).Set(float64(io.SyscW))
+			e.ioReadBytesGauge.WithLabelValues(labels...).Set(float64(io.ReadBytes))
+			e.ioWriteBytesGauge.WithLabelValues(labels...).Set(float64(io.WriteBytes))
+			e.ioCancelledWriteBytesGauge.WithLabelValues(labels...).Set(float64(io.CancelledWriteBytes))
+		}
+
+		// metrics data from /proc/$$/stat
+		// see https://github.com/prometheus/procfs/blob/master/proc_stat.go
+		e.minfltGauge.WithLabelValues(labels...).Set(float64(stat.MinFlt))
+		e.cminfltGauge.WithLabelValues(labels...).Set(float64(stat.CMinFlt))
+		e.majfltGauge.WithLabelValues(labels...).Set(float64(stat.MajFlt))
+		e.cmajfltGauge.WithLabelValues(labels...).Set(float64(stat.CMajFlt))
+		e.utimeGauge.WithLabelValues(labels...).Set(float64(stat.UTime))
+		e.stimeGauge.WithLabelValues(labels...).Set(float64(stat.STime))
+		e.cutimeGauge.WithLabelValues(labels...).Set(float64(stat.CUTime))
+		e.cstimeGauge.WithLabelValues(labels...).Set(float64(stat.CSTime))
+		e.niceGauge.WithLabelValues(labels...).Set(float64(stat.Nice))
+		e.numThreadsGauge.WithLabelValues(labels...).Set(float64(stat.NumThreads))
+		e.startTimeGauge.WithLabelValues(labels...).Set(e.bootTime + float64(stat.Starttime)/userHZ)
+		e.vsizeGauge.WithLabelValues(labels...).Set(float64(stat.VSize))
+		e.rssGauge.WithLabelValues(labels...).Set(float64(stat.RSS * e.pagesize))
+	}
+
+	e.ioRCharGauge.Collect(ch)
+	e.ioWCharGauge.Collect(ch)
+	e.ioSyscRGauge.Collect(ch)
+	e.ioSyscWGauge.Collect(ch)
+	e.ioReadBytesGauge.Collect(ch)
+	e.ioWriteBytesGauge.Collect(ch)
+	e.ioCancelledWriteBytesGauge.Collect(ch)
+	e.minfltGauge.Collect(ch)
+	e.cminfltGauge.Collect(ch)
+	e.majfltGauge.Collect(ch)
+	e.cmajfltGauge.Collect(ch)
+	e.utimeGauge.Collect(ch)
+	e.stimeGauge.Collect(ch)
+	e.cutimeGauge.Collect(ch)
+	e.cstimeGauge.Collect(ch)
+	e.niceGauge.Collect(ch)
+	e.numThreadsGauge.Collect(ch)
+	e.startTimeGauge.Collect(ch)
+	e.vsizeGauge.Collect(ch)
+	e.rssGauge.Collect(ch)
+
+	return nil
+}
+
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	e.mutex.Lock() // To protect metrics from concurrent collects.
+	defer e.mutex.Unlock()
+	if err := e.collect(ch); err != nil {
+		dbgf("Error scraping apache: %s", err)
+		e.scrapeFailures.Inc()
+		e.scrapeFailures.Collect(ch)
+	}
+	return
+}
+
+func dbg(a ...interface{}) {
+	if *verboseFlg {
+		log.Print(a...)
+	}
+}
+
+func dbgf(format string, v ...interface{}) {
+	if *verboseFlg {
+		log.Printf(format, v...)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if *versionFlg {
+		fmt.Fprintf(os.Stderr, "%s version %s\n", os.Args[0], version)
+
+		os.Exit(0)
+	}
+
+	exporter, err := NewExporter(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	prometheus.MustRegister(exporter)
 
 	// Expose the registered metrics via HTTP.
 	http.Handle("/metrics", prometheus.Handler())
